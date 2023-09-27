8c3ffdbadc1ac14034898f088f832cc29862baea
Ivan A <heysoyivan@gmail.com>
2023-09-28 00:54:31 +0900

    first commit fix neo-modules index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,20 @@
 // index.d.ts
 declare module 'neo-module' {
-    export function renderApp(): void;
-    // Define other exported functions and classes
+    export const renderApp: (app: () => Promise<{ elements: Element[] }>, container: HTMLElement) => void;
+    export const registerOnAppear: (callback: () => void) => void;
+    export const registerOnDisappear: (callback: () => void) => void;
+    export const vStack: (options: { spacing?: number; style?: Record<string, string> }, children: Element[]) => Element;
+    export const hStack: (options: { spacing?: number; style?: Record<string, string> }, children: Element[]) => Element;
+    export const text: (content: string, options?: { style?: Record<string, string> }) => Element;
+    export const button: (label: string, onClick: () => void, options?: { style?: Record<string, string> }) => Element;
+    export const input: (options?: { placeholder?: string; onChange?: (value: string) => void; style?: Record<string, string> }) => Element;
+    export const image: (src: string, options?: { style?: Record<string, string> }) => Element;
+    export const breadcrumb: (options?: { items?: string[]; separator?: string; style?: Record<string, string> }) => Element;
+
+    // Export the StateManager type if necessary
+    export const StateManager: {
+        subscribe: (callback: (data: any) => void) => void;
+        setState: (key: string | number, value: any) => void;
+        getState: (key: string | number) => any;
+    };
 }
--- index.ts
+++ index.ts
@@ -3,5 +3,5 @@ import Neo from './neo';
 import StateManager from './state';
 const { renderApp, registerOnAppear, registerOnDisappear, vStack, hStack, image } = Neo
 export {
-    renderApp, registerOnAppear, registerOnDisappear, vStack, hStack, image, StateManager
+    renderApp, registerOnAppear, registerOnDisappear, vStack, hStack, image, StateManager, Neo
 }
